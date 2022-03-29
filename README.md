ceed6413ef41e2a38f20f88c1706c13565601e5e
Alex Vlad <lupiulakilog@gmail.com>
2022-03-30 00:00:35 +0900

    User_M modified files again

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # Software_Engineering_HW01
 Version Control Software Report
 # USER_M 
-User_M added a function
+User_M added a function printMessage()
+User_M added a function printContacts()
--- UploadFile.java
+++ UploadFile.java
@@ -5,8 +5,14 @@ public class UploadFile{
     {
         System.out.println(msg);
     }
+
+    public static void printContacts (String msg)
+    {
+        System.out.println(msg);
+    }
     public static void main(String[] args) {
         printMesage("I like ICE AMERICANO!");
+        printContacts("kokok@fmail.com");
         return;
     }
 }
